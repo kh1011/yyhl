5e4bc904d57ff38d13c5918ccc0c2efaa73dbdfc
kh1011 <782818448@qq.com>
2022-08-03 11:23:51 +0800

    提交

--- prompts.js
+++ prompts.js
@@ -1,14 +1,3 @@
 module.exports = [
-  {
-    name: "vuex",
-    type: "confirm",
-    message: `是否需要使用 vuex`,
-    default: false
-  },
-  {
-    name: "elementUI",
-    type: "confirm",
-    message: `element-ui`,
-    default: false
-  }
+ 
 ];
